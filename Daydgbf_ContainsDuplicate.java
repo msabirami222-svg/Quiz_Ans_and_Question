142298d2b7ea9174fa4fc7858fae3a3762315a51
msabirami222-svg <msabirami222@gmail.com>
2026-01-28 16:07:08 +0530

    Refactor comments in Daydgbf_ContainsDuplicate.java
    Updated comments to use single-line format.

---
+++ Daydgbf_ContainsDuplicate.java
@@ -0,0 +1,20 @@
+// Contains Duplicate:
+// Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+// Example 1:
+// Input: nums = [1, 2, 3, 3]
+// Output: true
+// Example 2:
+// Input: nums = [1, 2, 3, 4]
+// Output: false
+
+class Daydgbf_ContainsDuplicate {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> set=new HashSet<>();
+        for(int val:nums){
+            if(!set.add(val)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
